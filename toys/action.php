f1ac3b4a30184583ec57159edf969cd27507a985
KINOSHITA Shuji <shuji-k@is.naist.jp>
2013-04-19 22:38:39 +0900

    ただtableにinsertするだけの処理を書いてみた。

--- toys/action.php
+++ toys/action.php
@@ -9,6 +9,27 @@
 </head>
 <body>
 <div class="small">
+<?php
+$con = mysql_connect("localhost","kino","7hueychan");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("kino", $con);
+
+$sql="INSERT INTO sample
+VALUES
+('$_POST[name]','$_POST[sex]','$_POST[blood]','$_POST[kanso]')";
+
+if (!mysql_query($sql,$con))
+  {
+  die('Error: ' . mysql_error());
+  }
+echo "1 record added";
+
+mysql_close($con)
+?><br/>
 こんにちは、<?php echo htmlspecialchars($_POST['name']); ?>さん。<br/>
 <?php echo $_POST['sex']; ?><br/>
 <?php echo $_POST['blood']; ?><br/>
